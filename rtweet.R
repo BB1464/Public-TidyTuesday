fb907b9caa00e0eaaadee65056521f4cb677016e
BB1464 <oluwafemioyedele908@gmail.com>
2023-01-05 19:29:52 +0100

    Updated my code for rtweet

--- rtweet.R
+++ rtweet.R
@@ -2,5 +2,5 @@
 library(rtweet)
 
 
-post_tweet(status = "#TidyTuesday  Week-01, #BYOD,I Visualize areas where we have more and less Chocolate using a treemap, this was my first attempt of using a treemap and my inspiration comes from #nrennie35,Happy New Data Year All!!! #rstat #dataviz #tidyverse",media = '2023/2023-01-05-Week-01/chocolate.png',media_alt_text = 'Chocolate Ratings')
+post_tweet(status = "#TidyTuesday  Week-01, #BYOD,I Visualize areas where we have more and less Chocolate using a treemap, this was my first attempt of using a treemap and my inspiration comes from #nrennie35,I just added more flare to the code,Happy New Data Year All!!! #rstat #dataviz #tidyverse, Code: https://github.com/BB1464/Public-TidyTuesday/tree/master/2023/2023-01-05-Week-01",media = '2023/2023-01-05-Week-01/chocolate.png',media_alt_text = 'Chocolate Ratings')
 
